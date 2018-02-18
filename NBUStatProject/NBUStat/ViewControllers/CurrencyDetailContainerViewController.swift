6c9d6c60d75121f5c44dd477e9ebc3dbb206b164
Oleksii Tiurenkov <alterego4@gmail.com>
2018-02-18 22:21:53 +0200

    Added charts library

--- NBUStatProject/NBUStat/ViewControllers/CurrencyDetailContainerViewController.swift
+++ NBUStatProject/NBUStat/ViewControllers/CurrencyDetailContainerViewController.swift
@@ -30,7 +30,6 @@ class CurrencyDetailContainerViewController: UIViewController {
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let container = segue.destination as? CurrencyDetailTableViewController
         {
