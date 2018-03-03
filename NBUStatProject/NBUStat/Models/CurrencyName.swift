1f08f5c2ec1c87326c67cc20fe2354469f32f4d3
Oleksii Tiurenkov <alterego4@gmail.com>
2018-03-04 01:03:11 +0200

    Fixed numbers in currency codes

--- NBUStatProject/NBUStat/Models/CurrencyName.swift
+++ NBUStatProject/NBUStat/Models/CurrencyName.swift
@@ -19,6 +19,16 @@ class CurrencyName
             return "Євро"
         case "643":
             return "Російський рубль"
+        case "124":
+            return "Канадський долар"
+        case "392":
+            return "Японська єна"
+        case "756":
+            return "Швейцарський франк"
+        case "826":
+            return "Англійський фунт стерлінгів"
+        case "985":
+            return "Польський злотий"
         default:
             return code
         }
