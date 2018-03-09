65629d2894380257c91f174b300704c129f8c764
Oleksii Tiurenkov <Oleksii_Tiurenkov@epam.com>
2018-03-09 14:39:49 +0200

    Added library

---
+++ NBUStatProject/FinstatLib/FinstatLib/FinstatLib.h
@@ -0,0 +1,19 @@
+//
+//  FinstatLib.h
+//  FinstatLib
+//
+//  Created by Oleksii Tiurenkov on 3/9/18.
+//  Copyright © 2018 Oleksii Tiurenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FinstatLib.
+FOUNDATION_EXPORT double FinstatLibVersionNumber;
+
+//! Project version string for FinstatLib.
+FOUNDATION_EXPORT const unsigned char FinstatLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FinstatLib/PublicHeader.h>
+
+
