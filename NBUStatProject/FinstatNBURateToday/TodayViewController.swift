7b1c320783a9cd67ee39d1f89536580ebf80cc88
Oleksii Tiurenkov <alterego4@gmail.com>
2018-03-18 19:15:31 +0200

    Added NBU rates today widget

--- NBUStatProject/FinstatNBURateToday/TodayViewController.swift
+++ NBUStatProject/FinstatNBURateToday/TodayViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 import NotificationCenter
+import FinstatLib
+
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
+    @IBOutlet weak var usdValue: UILabel!
+    @IBOutlet weak var eurValue: UILabel!
+    @IBOutlet weak var providerLabel: UILabel!
+    
+    private var service = NBURatesService()
     override func viewDidLoad() {
         super.viewDidLoad()
+        usdValue.text = "---"
+        eurValue.text = "---"
         // Do any additional setup after loading the view from its nib.
+
     }
+    
+    func update(label: UILabel, rate: [CurrencyRate])
+    {
+        guard rate.count == 1 else { return }
+        guard let rate = rate.first else { return }
+        label.text = rate.newRate.stringValue
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func openMainApplicationTapped(_ sender: Any) {
+        guard let mainApplicationURL = URL(string: "finstatukraine://nbu") else { return }
+        self.extensionContext?.open(mainApplicationURL, completionHandler: nil)
+    }
+    
     
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
@@ -27,8 +51,23 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
-        
-        completionHandler(NCUpdateResult.newData)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        let dateString = dateFormatter.string(from: Date())
+        service.loadList(param: dateString) { (rates, error) in
+            if error == nil
+            {
+                let rates = rates.filter{ return ($0.r030 == "840" || $0.r030 == "978") }
+                self.update(label: self.usdValue, rate: rates.filter{ $0.r030 == "840"});
+                self.update(label: self.eurValue, rate: rates.filter{ $0.r030 == "978"});
+                completionHandler(NCUpdateResult.newData)
+            }
+            else
+            {
+                completionHandler(NCUpdateResult.failed)
+            }
+        }
+
     }
     
 }
